80a83cd1f9ccf6f204e4b2a5c1329d585b74c0b0
Bryce McWilliams <brycemcwilliams@gmail.com>
2020-04-23 20:41:26 +0200

    chore: initial commit

---
+++ custom-knn-algorithm.py
@@ -0,0 +1,54 @@
+# Make Predictions with k-nearest neighbors on the Iris Flowers Dataset
+from math import sqrt
+from sklearn.datasets import load_iris
+ 
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+	minmax = list()
+	for i in range(len(dataset[0])):
+		col_values = [row[i] for row in dataset]
+		value_min = min(col_values)
+		value_max = max(col_values)
+		minmax.append([value_min, value_max])
+	return minmax
+ 
+# Rescale dataset columns to the range 0-1
+def normalize_dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)):
+			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+ 
+# Calculate the Euclidean distance between two vectors
+def euclidean_distance(row1, row2):
+	distance = 0.0
+	for i in range(len(row1)-1):
+		distance += (row1[i] - row2[i])**2
+	return sqrt(distance)
+ 
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+	distances = list()
+	for train_row in train:
+		dist = euclidean_distance(test_row, train_row)
+		distances.append((train_row, dist))
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+ 
+# Make a prediction with neighbors
+def predict_classification(train, test_row, num_neighbors):
+	neighbors = get_neighbors(train, test_row, num_neighbors)
+	output_values = [row[-1] for row in neighbors]
+	prediction = max(set(output_values), key=output_values.count)
+	return prediction
+ 
+# Make a prediction with KNN on Iris Dataset
+dataset = load_iris().data
+num_neighbors = 5
+# define a new record
+row = [5.7, 2.9, 4.2, 1.3]
+# predict the label
+label = predict_classification(dataset, row, num_neighbors)
+print('Data=%s, Predicted: %s' % (row, label))
